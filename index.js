7c57d1b513269843455fbc8c5e803a84a94a5478
Jason Ballejo <ballejo19@gmail.com>
2022-08-28 14:42:16 -0400

    commit tree 2cd42d95f823333de0160cef03c0252070da06bd

--- index.js
+++ index.js
@@ -1,4 +1,79 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeRec){
+    // console.log('First', employeeRec[3])
+
+    return {
+        firstName: employeeRec[0],
+        familyName: employeeRec[1],
+        title: employeeRec[2],
+        payPerHour: employeeRec[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeeRec){
+    return employeeRec.map(e => createEmployeeRecord(e))
+}
+
+function createTimeInEvent(dateStamp){
+    // console.log('DATE:', dateStamp)
+    // console.log('This:', this)
+
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    })
+
+    return this
+
+}
+
+function createTimeOutEvent(dateStamp) {
+    // console.log('THIS:', this)
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(dateStamp.split(' ')[1]),
+        date: dateStamp.split(' ')[0]
+    })
+
+    return this
+}
+
+function hoursWorkedOnDate(dateForm){
+    let dateHoursIn = this.timeInEvents.find(function (e) {
+        return e.date === dateForm
+    })
+    // console.log('HourIn:', dateHoursIn) // [ 900 ]
+
+    let dateHoursOut = this.timeOutEvents.find(function (e) {
+        return e.date === dateForm
+    })
+    // console.log('HourOut:', dateHoursOut) // [ 1100 ]
+
+    return (dateHoursOut.hour - dateHoursIn.hour) / 100
+}
+
+function wagesEarnedOnDate(dateForm){
+    return (hoursWorkedOnDate.call(this, dateForm) * this.payPerHour) // 2 * 27 = 54
+    // console.log('WORKED:', hoursWorkedOnDate.call(this, dateForm) * this.payPerHour)
+    // console.log('Function:', hoursWorkedOnDate.call(this, dateForm))
+    // console.log('PayRate:', this.payPerHour)
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    //`srcArray`: Array of employee records
+    //`firstName`: String representing a first name held in an employee record
+    // console.log("SRC:", srcArray)
+    console.log('FirstName:', srcArray[0].firstName)
+
+    // return srcArray[0].firstName === firstName
+    return srcArray.find(function (e){
+        return e.firstName === firstName
+    })
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +96,14 @@ const allWagesFor = function () {
     return payable
 }
 
+function calculatePayroll(employeeRec){
+    // console.log("RECORDS:", employeeRec)
+
+    return employeeRec.reduce((previousValue, currentValue) => previousValue + allWagesFor.call(currentValue),0)
+}
+//* **Returns**
+// * Sum of pay owed for **all** employees for all dates, as a number
+// * **Behavior**
+//   * Using `allWagesFor` for each of the employees, accumulate the value of
+//     all dates worked by the employee in the record used as context. Amount
+//     should be returned as a number.
